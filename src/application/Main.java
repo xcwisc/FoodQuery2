fd412da3196db6a23954459e82cc38b883f07179
Chang Xu <changx@royal-03.cs.wisc.edu>
2018-11-27 20:11:08 -0600

    Commit 2

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,12 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.geometry.Orientation;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 
 
 public class Main extends Application {
@@ -11,6 +14,61 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
+			Label label = new Label("Meal plan");
+			ToolBar toolbar = new ToolBar(label);
+			VBox foodItemsVbox = new VBox();
+			HBox foodDetailHbox = new HBox();
+			
+			
+			
+			foodDetailHbox.getChildren().addAll(
+					new Label("Item name           "),
+					new Label("Brand               "),
+					new Label("Calories   "),
+					new Label("Fat(g)   "),
+					new Label("Carbs(g)   "),
+					new Label("Fiber(g)   "),
+					new Label("Protein(g)"));
+			
+			HBox foodHbox = new HBox();
+			foodHbox.getChildren().addAll(
+					new Label("Balsamic Vinaigrette"),
+					new Label("Meijer"),
+					new Label("xx"),
+					new Label("xx"),
+					new Label("xx"),
+					new Label("xx"),
+					new Label("xx"));
+			
+			HBox foodHbox2 = new HBox();
+			foodHbox2.getChildren().addAll(
+					new Label("Strawberry Lowfat Yogurt"),
+					new Label("Essential Everyday"),
+					new Label("xx"),
+					new Label("xx"),
+					new Label("xx"),
+					new Label("xx"),
+					new Label("xx"));
+			
+			
+			
+//			ListView<String> foodDetail = new ListView<String>();
+//			foodDetail.setOrientation(Orientation.HORIZONTAL);
+//			foodDetail.
+			ScrollPane foodItemSP = new ScrollPane();
+			
+//			toolbar.getItems().push();
+			foodItemsVbox.getChildren().addAll(
+					new Label("Available Food Items"),
+					foodDetailHbox,
+					foodItemSP);
+		
+			foodItemSP.setVbarPolicy(ScrollBarPolicy.ALWAYS);
+			
+			foodItemSP.setContent(new VBox(foodHbox, foodHbox2));
+
+			root.setTop(toolbar);
+			root.setCenter(foodItemsVbox);
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
