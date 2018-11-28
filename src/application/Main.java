043a7b04bcfc5bcfd08643663f3cfb8554a3f599
arjunsachar <43893429+arjunsachar@users.noreply.github.com>
2018-11-27 21:32:27 -0600

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -25,35 +25,80 @@ public class Main extends Application {
 			ListView listname = new ListView(names);
 			listname.setOrientation(Orientation.HORIZONTAL);
 			*/
+
+			
+			
+			GridPane foodGridPane = new GridPane();
+			int counter = 0;
+			gridPane.add(new Label("Red Wine Vinegar"), 0, 0, 1, 1);
+			gridPane.add(new Label("Great Value"), 1, 0, 1, 1);
+			gridPane.add(new Label(" xx        "), 2, 0, 1, 1);
+			gridPane.add(new Label("xx    "), 3, 0, 1, 1);
+			gridPane.add(new Label(" xx    "), 4, 0, 1, 1);
+			gridPane.add(new Label("   xx    "), 5, 0, 1, 1);
+			gridPane.add(new Label("     xx    "), 6, 0, 1, 1);
+			
+			gridPane.add(new Label("Shredded Mozzarella Cheese"), 0, 1, 1, 1);
+			gridPane.add(new Label("Meijer  "), 1, 1, 1, 1);
+			gridPane.add(new Label(" xx        "), 2, 1, 1, 1);
+			gridPane.add(new Label("xx    "), 3, 1, 1, 1);
+			gridPane.add(new Label(" xx    "), 4, 1, 1, 1);
+			gridPane.add(new Label("   xx    "), 5, 1, 1, 1);
+			gridPane.add(new Label("     xx    "), 6, 1, 1, 1);
+			
+			gridPane.add(new Label("Balsamic Vinaigrette"), 0, 2, 1, 1);
+			gridPane.add(new Label("Meijer  "), 1, 2, 1, 1);
+			gridPane.add(new Label(" xx        "), 2, 2, 1, 1);
+			gridPane.add(new Label("xx    "), 3, 2, 1, 1);
+			gridPane.add(new Label(" xx    "), 4, 2, 1, 1);
+			gridPane.add(new Label("   xx    "), 5, 2, 1, 1);
+			gridPane.add(new Label("     xx    "), 6, 2, 1, 1);
+			
+			gridPane.add(new Label("Strawberry LowFat Yogurt"), 0, 3, 1, 1);
+			gridPane.add(new Label("Essential Everyday"), 1, 3, 1, 1);
+			gridPane.add(new Label(" xx        "), 2, 3, 1, 1);
+			gridPane.add(new Label("xx    "), 3, 3, 1, 1);
+			gridPane.add(new Label(" xx    "), 4, 3, 1, 1);
+			gridPane.add(new Label("   xx    "), 5, 3, 1, 1);
+			gridPane.add(new Label("     xx    "), 6, 3, 1, 1);
+			
+			gridPane.add(new Label("Burrito Bean Cheese"), 0, 4, 1, 1);
+			gridPane.add(new Label("Don Miguel"), 1, 4, 1, 1);
+			gridPane.add(new Label(" xx        "), 2, 4, 1, 1);
+			gridPane.add(new Label("xx    "), 3, 4, 1, 1);
+			gridPane.add(new Label(" xx    "), 4, 4, 1, 1);
+			gridPane.add(new Label("   xx    "), 5, 4, 1, 1);
+			gridPane.add(new Label("     xx    "), 6, 4, 1, 1);
+			
+			gridPane.add(new Label("Fat Free Skim Milk"), 0, 5, 1, 1);
+			gridPane.add(new Label("Kemps"), 1, 5, 1, 1);
+			gridPane.add(new Label(" xx        "), 2, 5, 1, 1);
+			gridPane.add(new Label("xx    "), 3, 5, 1, 1);
+			gridPane.add(new Label(" xx    "), 4, 5, 1, 1);
+			gridPane.add(new Label("   xx    "), 5, 5, 1, 1);
+			gridPane.add(new Label("     xx    "), 6, 5, 1, 1);
+			
+			gridPane.add(new Label("Greek Yogurt Lemon"), 0, 6, 1, 1);
+			gridPane.add(new Label("Yoplait"), 1, 6, 1, 1);
+			gridPane.add(new Label(" xx        "), 2, 6, 1, 1);
+			gridPane.add(new Label("xx    "), 3, 6, 1, 1);
+			gridPane.add(new Label(" xx    "), 4, 6, 1, 1);
+			gridPane.add(new Label("   xx    "), 5, 6, 1, 1);
+			gridPane.add(new Label("     xx    "), 6, 6, 1, 1);
+			
+			gridPane.setHgap(10);
+			
+			
 			foodDetailHbox.getChildren().addAll(
-					new Label("Item name              "),
-					new Label("Brand                  "),
-					new Label("Calories     "),
-					new Label("Fat(g)       "),
-					new Label("Carbs(g)     "),
-					new Label("Fiber(g)     "),
-					new Label("Protein(g)   "));//listname);
+					new Label("Item name                              "),
+					new Label(" Brand                        "),
+					new Label("Calories  "),
+					new Label("Fat(g) "),
+					new Label("Carbs(g) "),
+					new Label("Fiber(g) "),
+					new Label("Protein(g) ");
 			
-			HBox foodHbox = new HBox();
-			foodHbox.getChildren().addAll(
-					new Label("Balsamic Vinaigrette"),
-					new Label("Meijer"),
-					new Label("xx"),
-					new Label("xx"),
-					new Label("xx"),
-					new Label("xx"),
-					new Label("xx"));
-			
-			HBox foodHbox2 = new HBox();
-			foodHbox2.getChildren().addAll(
-					new Label("Strawberry Lowfat Yogurt"),
-					new Label("Essential Everyday"),
-					new Label("xx"),
-					new Label("xx"),
-					new Label("xx"),
-					new Label("xx"),
-					new Label("xx"));
-			
+	
 			
 			
 //			ListView<String> foodDetail = new ListView<String>();
@@ -69,7 +114,7 @@ public class Main extends Application {
 		
 			foodItemSP.setVbarPolicy(ScrollBarPolicy.ALWAYS);
 			
-			foodItemSP.setContent(new VBox(foodHbox, foodHbox2));
+			foodItemSP.setContent(foodGridPane);
 
 			root.setTop(toolbar);
 			root.setCenter(foodItemsVbox);
