19a7a82954e15eb32f920289cfa2c5ee8b8a6f49
Bennett Majerowski <majerowski@royal-29.cs.wisc.edu>
2018-11-30 16:51:50 -0600

    added some wrapper class that doesn't work.

---
+++ src/application/RulesWrapper.java
@@ -0,0 +1,21 @@
+package application;
+
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+
+public class RulesWrapper {
+	private HBox rule;
+	
+	public RulesWrapper(String attribute, String comparison, Double number) {
+		Text attributeText = new Text(attribute);
+		Text comparisonText = new Text(comparison);
+		Text numberText = new Text(number.toString());
+		Button deleteButton = new Button("Delete Rule");
+		rule.getChildren().addAll(attributeText, comparisonText, numberText, deleteButton);
+	}
+	
+	public HBox getComponent() {
+		return rule;
+	}
+}
