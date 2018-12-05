01ea7e4f49cf106eee809cc3d4554fcc7b998a32
Micah Jona <jona@royal-28.cs.wisc.edu>
2018-12-04 20:13:33 -0600

    implement filterByNutrient in foodData

--- src/application/FoodData.java
+++ src/application/FoodData.java
@@ -1,4 +1,5 @@
 package application;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -14,115 +15,196 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- * This class represents the backend for managing all 
- * the operations associated with FoodItems
+ * This class represents the backend for managing all the operations associated
+ * with FoodItems
  * 
  * @author sapan (sapan@cs.wisc.edu)
  */
 public class FoodData implements FoodDataADT<FoodItem> {
-    
-    // List of all the food items.
-    private List<FoodItem> foodItemList;
 
-    // Map of nutrients and their corresponding index
-    private HashMap<String, BPTree<Double, FoodItem>> indexes;
-    
-    
-    /**
-     * Public constructor
-     */
-    public FoodData() {
-        // TODO : Complete
-    	this.foodItemList = new ArrayList<FoodItem>();
-    	this.indexes = new HashMap<String, BPTree<Double, FoodItem>>();
-    }
-    
-    
-    /*
-     * (non-Javadoc)
-     * @see skeleton.FoodDataADT#loadFoodItems(java.lang.String)
-     */
-    @Override
-    public void loadFoodItems(String filePath) {
-        // TODO : Complete
-    	try {
-    	      File inputF = new File(filePath);
+	// List of all the food items.
+	private List<FoodItem> foodItemList;
 
-    	      InputStream inputFS = new FileInputStream(inputF);
+	// Map of nutrients and their corresponding index
+	private HashMap<String, BPTree<Double, FoodItem>> indexes;
 
-    	      BufferedReader br = new BufferedReader(new InputStreamReader(inputFS));
-    	      // the filter method may need improvement
-    	      this.foodItemList = br.lines().filter(data -> data.length() > 11).map(rule -> parser(rule)).collect(Collectors.toList());
-    	      
-    	      //TODO: construct the HashMap
-    	      br.close();
-    	} catch(Exception e) {
-    		e.printStackTrace();
-            System.out.println(e);
-    	}
-    }
-    
-    //556540ff5d613c9d5f5935a9,Stewarts_PremiumDarkChocolatewithMintCookieCrunch,calories,280,fat,18,carbohydrate,34,fiber,3,protein,3
-    private FoodItem parser(String data) {
-//    	System.out.println(data);
-    	String[] parts = data.split(",");
-    	String id = parts[0];
-    	String[] wholeName = parts[1].split("_");
-    	String itemName = wholeName[1];
-    	String brand = wholeName[0];
-    	String calories = parts[3];
-    	String fat = parts[5];
-    	String carbs = parts[7];
-    	String fiber = parts[9];
-    	String protein = parts[11];
-    	FoodItem ans = new FoodItem(id, itemName, brand, calories, fat, carbs, fiber, protein);
-    	return ans;
-//    	return null;
-    }
+	/**
+	 * Public constructor
+	 */
+	public FoodData() {
+		// TODO : Complete
+		this.foodItemList = new ArrayList<FoodItem>();
+		this.indexes = new HashMap<String, BPTree<Double, FoodItem>>();
+	}
 
-    /*
-     * (non-Javadoc)
-     * @see skeleton.FoodDataADT#filterByName(java.lang.String)
-     */
-    @Override
-    public List<FoodItem> filterByName(String substring) {
-    	List<FoodItem> result = new ArrayList<FoodItem>();
-    	this.foodItemList.forEach(item -> {
-    		if (item.getFullName().toLowerCase().contains(substring.toLowerCase())) {
-    			result.add(item);
-    		}
-    	});
-        return result;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see skeleton.FoodDataADT#loadFoodItems(java.lang.String)
+	 */
+	@Override
+	public void loadFoodItems(String filePath) {
+		// TODO : Complete
+		try {
+			File inputF = new File(filePath);
 
-    /*
-     * (non-Javadoc)
-     * @see skeleton.FoodDataADT#filterByNutrients(java.util.List)
-     */
-    @Override
-    public List<FoodItem> filterByNutrients(List<String> rules) {
-        // TODO : Complete
-        return null;
-    }
+			InputStream inputFS = new FileInputStream(inputF);
 
-    /*
-     * (non-Javadoc)
-     * @see skeleton.FoodDataADT#addFoodItem(skeleton.FoodItem)
-     */
-    @Override
-    public void addFoodItem(FoodItem foodItem) {
-    	this.foodItemList.add(foodItem);
-    }
+			BufferedReader br = new BufferedReader(new InputStreamReader(inputFS));
+			// the filter method may need improvement
+			this.foodItemList = br.lines().filter(data -> data.length() > 11).map(rule -> parser(rule))
+					.collect(Collectors.toList());
 
-    /*
-     * (non-Javadoc)
-     * @see skeleton.FoodDataADT#getAllFoodItems()
-     */
-    @Override
-    public List<FoodItem> getAllFoodItems() {
-    	return this.foodItemList;
-    }
+			BPTree<Double, FoodItem> BPTreeCalories = new BPTree<Double, FoodItem>(3);
+			for (FoodItem item : this.foodItemList) {
+				BPTreeCalories.insert(item.getNutrientValue("calories"), item);
+			}
+			this.indexes.put("calories", BPTreeCalories);
+			
+			BPTree<Double, FoodItem> BPTreeFat = new BPTree<Double, FoodItem>(3);
+			for (FoodItem item : this.foodItemList) {
+				BPTreeFat.insert(item.getNutrientValue("fat"), item);
+			}
+			this.indexes.put("fat", BPTreeFat);
+			
+			BPTree<Double, FoodItem> BPTreeCarbs = new BPTree<Double, FoodItem>(3);
+			for (FoodItem item : this.foodItemList) {
+				BPTreeCarbs.insert(item.getNutrientValue("carbs"), item);
+			}
+			this.indexes.put("carbs", BPTreeCarbs);
+			
+			BPTree<Double, FoodItem> BPTreeFiber = new BPTree<Double, FoodItem>(3);
+			for (FoodItem item : this.foodItemList) {
+				BPTreeCalories.insert(item.getNutrientValue("fiber"), item);
+			}
+			this.indexes.put("fiber", BPTreeFiber);
+			
+			BPTree<Double, FoodItem> BPTreeProtein = new BPTree<Double, FoodItem>(3);
+			for (FoodItem item : this.foodItemList) {
+				BPTreeCalories.insert(item.getNutrientValue("protein"), item);
+			}
+			this.indexes.put("protein", BPTreeProtein);
+			
+			
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e);
+		}
+	}
 
+	// 556540ff5d613c9d5f5935a9,Stewarts_PremiumDarkChocolatewithMintCookieCrunch,calories,280,fat,18,carbohydrate,34,fiber,3,protein,3
+	private FoodItem parser(String data) {
+		// System.out.println(data);
+		String[] parts = data.split(",");
+		String id = parts[0];
+		String[] wholeName = parts[1].split("_");
+		String itemName = wholeName[1];
+		String brand = wholeName[0];
+		String calories = parts[3];
+		String fat = parts[5];
+		String carbs = parts[7];
+		String fiber = parts[9];
+		String protein = parts[11];
+		FoodItem ans = new FoodItem(id, itemName, brand, calories, fat, carbs, fiber, protein);
+		return ans;
+		// return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see skeleton.FoodDataADT#filterByName(java.lang.String)
+	 */
+	@Override
+	public List<FoodItem> filterByName(String substring) {
+		List<FoodItem> result = new ArrayList<FoodItem>();
+		this.foodItemList.forEach(item -> {
+			if (item.getFullName().toLowerCase().contains(substring.toLowerCase())) {
+				result.add(item);
+			}
+		});
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see skeleton.FoodDataADT#filterByNutrients(java.util.List)
+	 */
+	@Override
+	public List<FoodItem> filterByNutrients(List<String> rules) {
+		// TODO : Complete
+		// "<nutrient> <comparator> <value>"
+		// calories > 200
+		
+		// answer list to return
+		List<FoodItem> ans = new ArrayList<FoodItem>();
+		
+		// making a copy of the indexes hashmap
+		// !!!! this is a shallow copy
+		HashMap<String, BPTree<Double, FoodItem>> indexesCopy = (HashMap<String, BPTree<Double, FoodItem>>) this.indexes.clone();
+
+		for (int i = 0; i < rules.size(); i++) {
+			String parts[] = rules.get(i).split(" ");
+			String nutrient = parts[0];
+			String comparator = parts[1];
+			double value = Double.parseDouble(parts[2]);
+			// update the list
+			ans = indexesCopy.get(nutrient).rangeSearch(value, comparator);
+			// update the hashMap
+			BPTree<Double, FoodItem> BPTreeCalories = new BPTree<Double, FoodItem>(3);
+			for (FoodItem item : this.foodItemList) {
+				BPTreeCalories.insert(item.getNutrientValue("calories"), item);
+			}
+			indexesCopy.put("calories", BPTreeCalories);
+			
+			BPTree<Double, FoodItem> BPTreeFat = new BPTree<Double, FoodItem>(3);
+			for (FoodItem item : this.foodItemList) {
+				BPTreeFat.insert(item.getNutrientValue("fat"), item);
+			}
+			indexesCopy.put("fat", BPTreeFat);
+			
+			BPTree<Double, FoodItem> BPTreeCarbs = new BPTree<Double, FoodItem>(3);
+			for (FoodItem item : this.foodItemList) {
+				BPTreeCarbs.insert(item.getNutrientValue("carbs"), item);
+			}
+			indexesCopy.put("carbs", BPTreeCarbs);
+			
+			BPTree<Double, FoodItem> BPTreeFiber = new BPTree<Double, FoodItem>(3);
+			for (FoodItem item : this.foodItemList) {
+				BPTreeCalories.insert(item.getNutrientValue("fiber"), item);
+			}
+			indexesCopy.put("fiber", BPTreeFiber);
+			
+			BPTree<Double, FoodItem> BPTreeProtein = new BPTree<Double, FoodItem>(3);
+			for (FoodItem item : this.foodItemList) {
+				BPTreeCalories.insert(item.getNutrientValue("protein"), item);
+			}
+			indexesCopy.put("protein", BPTreeProtein);
+		}
+		return ans;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see skeleton.FoodDataADT#addFoodItem(skeleton.FoodItem)
+	 */
+	@Override
+	public void addFoodItem(FoodItem foodItem) {
+		this.foodItemList.add(foodItem);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see skeleton.FoodDataADT#getAllFoodItems()
+	 */
+	@Override
+	public List<FoodItem> getAllFoodItems() {
+		return this.foodItemList;
+	}
 
 	@Override
 	public void saveFoodItems(String filename) {
@@ -137,24 +219,24 @@ public class FoodData implements FoodDataADT<FoodItem> {
 			foodItemList.forEach(item -> {
 				foodDataString.add(item.getString());
 			});
-			for(String str: foodDataString) {
+			for (String str : foodDataString) {
 				writer.write(str);
 			}
 			writer.close();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-            System.out.println(e);
+			System.out.println(e);
 		}
 	}
-	
-//	// Test
-//	public static void main(String[] args) {
-//		FoodData foodData = new FoodData();
-//		foodData.loadFoodItems("/Users/cxu249/eclipse-workspace/FoodQuery/foodItems.csv");
-//		foodData.saveFoodItems("/Users/cxu249/Desktop/hi.txt");
-//		List<FoodItem> filtered = foodData.filterByName("Sour");
-//		for (FoodItem item: filtered) {
-//			System.out.println(item.getString());
-//		}
-//	}
+
+	// Test
+	public static void main(String[] args) {
+		FoodData foodData = new FoodData();
+		foodData.loadFoodItems("/u/j/o/jona/Desktop/foodItems.csv");
+		foodData.saveFoodItems("/u/j/o/jona/Desktop/test.txt");
+		List<FoodItem> filtered = foodData.filterByName("Sour");
+		for (FoodItem item : filtered) {
+			System.out.println(item.getString());
+		}
+	}
 }
--- src/application/FoodItem.java
+++ src/application/FoodItem.java
@@ -1,4 +1,5 @@
 package application;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,140 +11,142 @@ import java.util.List;
  */
 public class FoodItem {
 
-    // The id of the food item.
-    private String id;
-    // The name of the food item.
-    private String itemName;
-    private String brand;
-    private String calories; 
-    private String fat;
-    private String carbs;
-    private String fiber;
-    private String protein;
+	// The id of the food item.
+	private String id;
+	// The name of the food item.
+	private String itemName;
+	private String brand;
+	private String calories;
+	private String fat;
+	private String carbs;
+	private String fiber;
+	private String protein;
 
-    
-    //String calories, String fat, String carbs, String fiber, String protein
-    
-    /**
-     * Constructor
-     * @param name name of the food item
-     * @param id unique id of the food item
-     */
+	// String calories, String fat, String carbs, String fiber, String protein
 
+	/**
+	 * Constructor
+	 * 
+	 * @param name
+	 *            name of the food item
+	 * @param id
+	 *            unique id of the food item
+	 */
 
+	// Map of nutrients and value.
+	private HashMap<String, Double> nutrients;
 
-    // Map of nutrients and value.
-    private HashMap<String, Double> nutrients;
-    
-    /**
-     * Constructor
-     * @param name name of the food item
-     * @param id unique id of the food item 
-     */
-    public FoodItem(String id, String itemName, String brand, String calories, String fat, String carbs, String fiber, String protein) {
-        // TODO : Complete
-    	this.id = id;
-        this.itemName = new String(itemName);
-        this.brand = brand;
-        this.calories = calories;
-        this.fat = fat;
-        this.carbs = carbs;
-        this.fiber = fiber;
-        this.protein = protein;
-        this.nutrients = new HashMap<String, Double>();
-        this.nutrients.put("calories", Double.parseDouble(this.calories));
-        this.nutrients.put("fat", Double.parseDouble(this.fat));
-        this.nutrients.put("carbs", Double.parseDouble(this.carbs));
-        this.nutrients.put("fiber", Double.parseDouble(this.fiber));
-        this.nutrients.put("protein", Double.parseDouble(this.protein));
-    }
-    
-   
-    
-    
-    /**
-     * Gets the name of the food item
-     * 
-     * @return name of the food item
-     */
-    public String getItemName() {
-        return this.itemName;
-    }
+	/**
+	 * Constructor
+	 * 
+	 * @param name
+	 *            name of the food item
+	 * @param id
+	 *            unique id of the food item
+	 */
+	public FoodItem(String id, String itemName, String brand, String calories, String fat, String carbs, String fiber,
+			String protein) {
+		// TODO : Complete
+		this.id = id;
+		this.itemName = new String(itemName);
+		this.brand = brand;
+		this.calories = calories;
+		this.fat = fat;
+		this.carbs = carbs;
+		this.fiber = fiber;
+		this.protein = protein;
+		this.nutrients = new HashMap<String, Double>();
+		this.nutrients.put("calories", Double.parseDouble(this.calories));
+		this.nutrients.put("fat", Double.parseDouble(this.fat));
+		this.nutrients.put("carbs", Double.parseDouble(this.carbs));
+		this.nutrients.put("fiber", Double.parseDouble(this.fiber));
+		this.nutrients.put("protein", Double.parseDouble(this.protein));
+	}
 
-    /**
-     * Gets the unique id of the food item
-     * 
-     * @return id of the food item
-     */
-    public String getBrand() {
-        return this.brand;
-    }
-    
-    public String getCalories() {
-    	return this.calories;
-    }
-    
-    public String getFat() {
-    	return this.fat;
-    }
-    
-    public String getCarbs() {
-    	return this.carbs;
-    }
-    
-    public String getFiber() {
-    	return this.fiber;
-    }
-    
-    public String getProtein() {
-    	return this.protein;
-    }
-    
-    /**
-     * Gets the nutrients of the food item
-     * 
-     * @return nutrients of the food item
-     */
-    public HashMap<String, Double> getNutrients() {
-        return this.nutrients;
-    }
+	/**
+	 * Gets the name of the food item
+	 * 
+	 * @return name of the food item
+	 */
+	public String getItemName() {
+		return this.itemName;
+	}
 
-    /**
-     * Adds a nutrient and its value to this food. 
-     * If nutrient already exists, updates its value.
-     */
-    public void addNutrient(String name, double value) {
-    	this.nutrients.put(name, value);
-    	
-        // TODO : !!!!!!Complete: update the nutrient fields for the observable list to update
-    }
+	/**
+	 * Gets the unique id of the food item
+	 * 
+	 * @return id of the food item
+	 */
+	public String getBrand() {
+		return this.brand;
+	}
 
-    /**
-     * Returns the value of the given nutrient for this food item. 
-     * If not present, then returns 0.
-     */
-    public double getNutrientValue(String name) {
-        return this.nutrients.get(name);
-    }
-    
-    /**
-     * get the string to print the foodItem in the format of
-     * <id>,<food_name>,<calories>,<calorie_count>,<fat>,<fat_grams>,<carbohydrate>,<carbohydrate_grams>,<fiber>,<fiber_grams>,<protein>,<protein_grams>
-     * 556540ff5d613c9d5f5935a9,Stewarts_PremiumDarkChocolatewithMintCookieCrunch,calories,280,fat,18,carbohydrate,34,fiber,3,protein,3
-     */
-    public String getString() {
-    	String result = this.id + "," + this.brand + "_" + this.itemName + ",calories," + this.calories
-    			+ ",fat," + this.fat + ",carbohydrate," + this.carbs + ",fiber," + this.fiber + ",protein," + this.protein;
-    	return result;
-    }
-    
-    /**
-     * get fullName of the item for name filtering
-     * @return the fullName containing both the brand and the name
-     */
-    public String getFullName() {
-    	String result = this.brand + "_" + this.itemName;
-    	return result;
-    }
-    
+	public String getCalories() {
+		return this.calories;
+	}
+
+	public String getFat() {
+		return this.fat;
+	}
+
+	public String getCarbs() {
+		return this.carbs;
+	}
+
+	public String getFiber() {
+		return this.fiber;
+	}
+
+	public String getProtein() {
+		return this.protein;
+	}
+
+	/**
+	 * Gets the nutrients of the food item
+	 * 
+	 * @return nutrients of the food item
+	 */
+	public HashMap<String, Double> getNutrients() {
+		return this.nutrients;
+	}
+
+	/**
+	 * Adds a nutrient and its value to this food. If nutrient already exists,
+	 * updates its value.
+	 */
+	public void addNutrient(String name, double value) {
+		this.nutrients.put(name, value);
+	}
+
+	/**
+	 * Returns the value of the given nutrient for this food item. If not present,
+	 * then returns 0.
+	 */
+	public double getNutrientValue(String name) {
+		if (this.nutrients.containsKey(name))
+			return this.nutrients.get(name).doubleValue();
+		return 0;
+	}
+
+	/**
+	 * get the string to print the foodItem in the format of
+	 * <id>,<food_name>,<calories>,<calorie_count>,<fat>,<fat_grams>,<carbohydrate>,<carbohydrate_grams>,<fiber>,<fiber_grams>,<protein>,<protein_grams>
+	 * 556540ff5d613c9d5f5935a9,Stewarts_PremiumDarkChocolatewithMintCookieCrunch,calories,280,fat,18,carbohydrate,34,fiber,3,protein,3
+	 */
+	public String getString() {
+		String result = this.id + "," + this.brand + "_" + this.itemName + ",calories," + this.calories + ",fat,"
+				+ this.fat + ",carbohydrate," + this.carbs + ",fiber," + this.fiber + ",protein," + this.protein;
+		return result;
+	}
+
+	/**
+	 * get fullName of the item for name filtering
+	 * 
+	 * @return the fullName containing both the brand and the name
+	 */
+	public String getFullName() {
+		String result = this.brand + "_" + this.itemName;
+		return result;
+	}
+
 }
